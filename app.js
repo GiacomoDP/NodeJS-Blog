188c9cea503accb4a41d58b9f45c7f992088d504
Giacomo <giacomo.depol@hotmail.it>
2023-11-11 12:59:05 +0100

    connect MongoDB

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
+
 const app = express();
 const PORT = process.env.PORT || 6500;
+
+//Connect Database
+connectDB();
 
 app.use(express.static("public"));
 
